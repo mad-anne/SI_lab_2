96b249416f6877d1db272cadf93c6005682956ce
Anna Siwik <221026@student.pwr.edu.pl>
2017-04-17 19:46:46 +0200

    Harmonious Constraints working with constraints classes

---
+++ src/constraints/header/PairConstraint.h
@@ -0,0 +1,14 @@
+//
+// Created by Dom on 2017-04-17.
+//
+
+#ifndef SI_LAB_2_PAIRCONSTRAINT_H
+#define SI_LAB_2_PAIRCONSTRAINT_H
+
+
+class PairConstraint {
+
+};
+
+
+#endif //SI_LAB_2_PAIRCONSTRAINT_H
---
+++ src/constraints/source/PairConstraint.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dom on 2017-04-17.
+//
+
+#include "PairConstraint.h"
